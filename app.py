e093228c7bb139b5bfc9c43e7d9c1410a4e7ffd1
Subhasree Mohapatra <52351056+Eva86271@users.noreply.github.com>
2021-08-05 18:41:20 +1000

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict():
     For rendering results on HTML GUI
     '''
     review = [ x for x in request.form.values()]
-    refined_review = preprocess.text_preprocess(review)
+    refined_review = preprocess.text_preprocess(review[0])
     refined_review=cv.transform(refined_review)
     prediction = model.predict(refined_review)
     
