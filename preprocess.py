08d3895403856d22ef7b2ac18da9775f1488d39d
Subhasree Mohapatra <52351056+Eva86271@users.noreply.github.com>
2021-08-05 15:06:43 +1000

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud
+from nltk.stem import PorterStemmer
+#create an object of class PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from sklearn import feature_extraction
+porter = PorterStemmer()
+
+def text_preprocess(item):
+   lem=WordNetLemmatizer()
+   review=re.sub(r"[^a-zA-Z]",' ', item)
+   review=review.lower()  
+   review=nltk.sent_tokenize(review)
+   words=[nltk.word_tokenize(sent) for sent in review]
+   #for i in range(len(words)):
+     # words[i]=[lem.lemmatize(word) for word in words[i] if word not in stopwords.words('english')]    
+   words=[' '.join(word) for word in words]
+   print(words)
+   transformed_review=td.transform(words)
+   return transformed_review
