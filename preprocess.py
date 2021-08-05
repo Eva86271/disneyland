8212acf40928fd26d567479ac2909f5944f6f65a
Subhasree Mohapatra <52351056+Eva86271@users.noreply.github.com>
2021-08-05 15:09:57 +1000

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-from wordcloud import WordCloud
+import nltk
 from nltk.stem import PorterStemmer
 #create an object of class PorterStemmer
 from nltk.stem import WordNetLemmatizer
