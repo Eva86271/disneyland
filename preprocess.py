1bdad5d4a621c018cf8b832419ab338028f2f187
Subhasree Mohapatra <52351056+Eva86271@users.noreply.github.com>
2021-08-05 20:24:58 +1000

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import nltk
+nltk.download('punkt')
 import re
 from nltk.stem import PorterStemmer
 #create an object of class PorterStemmer
