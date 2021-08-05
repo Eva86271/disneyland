bfbdc06b260fc30f5835e82c7072ea1c2ee45ba6
Subhasree Mohapatra <52351056+Eva86271@users.noreply.github.com>
2021-08-05 16:58:49 +1000

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import nltk
+import re
 from nltk.stem import PorterStemmer
 #create an object of class PorterStemmer
 from nltk.stem import WordNetLemmatizer
